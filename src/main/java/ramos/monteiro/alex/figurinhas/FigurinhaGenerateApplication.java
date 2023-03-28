de0effd95fe2e443958d02a28bfd4fd9193f67a5
Alex Monteiro Ramos <Alex.M.Ramos@hotmail.com>
2023-03-27 21:34:14 -0300

    first commit

---
+++ src/main/java/ramos/monteiro/alex/figurinhas/FigurinhaGenerateApplication.java
@@ -0,0 +1,13 @@
+package ramos.monteiro.alex.figurinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigurinhaGenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigurinhaGenerateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ramos/monteiro/alex/figurinhas/FigurinhaGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package ramos.monteiro.alex.figurinhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FigurinhaGenerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
